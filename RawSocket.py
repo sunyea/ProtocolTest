1be112bf186679a0a34ab75652c16775356eac1a
sunyea <7192506@qq.com>
2019-01-16 18:03:43 +0800

    modify socket

--- RawSocket.py
+++ RawSocket.py
@@ -11,76 +11,214 @@ import socket
 import struct
 import random
 
+def decodeall(buff):
+    s = None
+    try:
+        s = buff.decode('utf-8')
+        return s
+    except:
+        pass
+    try:
+        s = buff.decode('unicode')
+        return s
+    except:
+        pass
+    try:
+        s = buff.decode('gb2312')
+        return s
+    except:
+        pass
+    return s
 
-def SendPacketData(Buffer=None, DestIP='127.0.0.1', DestPort=0):
-    if Buffer is None:
-        return False
+
+def CreateSocket():
     try:
         Socket = socket.socket(socket.AF_INET, socket.SOCK_RAW)
         Socket.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
         Socket.setsockopt(socket.SOL_SOCKET, socket.SO_SNDTIMEO, 2000)
-    except:
+        return Socket
+    except Exception as e:
         Socket.close()
-        return False
+        print('创建socket失败：{}'.format(e))
+        return None
 
+
+def GetIPHeader(SourceIP, DestIP, CheckSum=0):
+    '''
+    获取IP首部
+    :param SourceIP: 源地址
+    :param DestIP: 目的地址
+    :return:
+    '''
     try:
-        Socket.sendto(Buffer, 0, (DestIP, DestPort))
-    except:
-        Socket.close()
+        # 4 版本
+        version = 4
+        # 4 首部长度
+        head_len = 5
+        ver_head_len = (version << 4)+head_len
 
-    return True
+        # 8 服务类型
+        server = 0
+        # 16 总长度
+        tot_len = 20 + 20
+        # 16 标识
+        id = random.randint(18000, 65535)
+        # 3 标志
+        flag = 0
+        # 13 片偏移
+        offset = 0
+        flag_offset = (flag << 13)+offset
 
+        # 8 生存期
+        ttl = 255
+        # 8 协议
+        protocol = socket.IPPROTO_TCP
+        # 16 首部验证和
+        check_sum = CheckSum
+        # 32 源地址
+        source_ip = socket.inet_aton(SourceIP)
+        # 32 目的地址
+        dest_ip = socket.inet_aton(DestIP)
 
-def SetPacketAddress(Number=1):
-    return ['.'.join(['{}'.format(random.randint(1, 0xFF)) for i in range(0, 4)]) for n in range(0, Number)]
-
-
-def SetPacketData(Length=32):
-    return ''.join(['{}'.format(chr(random.randint(0, 255))) for n in range(Length)])
-
-
-def SetPacketCheckSum(Buffer=None):
-    if Buffer is None:
-        return False
-    if len(Buffer) % 2:
-        Buffer += b'0'
-    return ~sum([(ord(Buffer[n+1]) << 8) + ord(Buffer[n]) for n in range(0, len(Buffer), 2)])
-
-
-def SynSendInit(DestIP='127.0.0.1', DestPort=80):
-    IpHdrLen = 20
-    TcpHdrLen = 20
-
-    IpVerLen = (4<<4|int(IpHdrLen/4))
-    IpTotal_len = socket.htons(IpHdrLen + TcpHdrLen)
-
-    IpDestIP = struct.unpack('i', socket.inet_aton(DestIP))[0]
-    IpSourceIP = struct.unpack('i', socket.inet_aton(SetPacketAddress()[0]))[0]
-
-    TcpSport = socket.htons(random.randint(1024, 65000))
-    TcpDport = socket.htons(DestPort)
-
-    TcpLenres = (int(TcpHdrLen/4) << 4|0)
-    TcpSeq = socket.htonl(int(time.time()))
-
-    Buffer = struct.pack('LLBBHHHLLBBHHH', IpSourceIP, IpDestIP, 0, socket.IPPROTO_TCP, socket.htons(TcpHdrLen), TcpSport, TcpDport, TcpSeq, 0, TcpLenres, 2, socket.htons(8192), 0, 0)
-    TcpChecksum = SetPacketCheckSum(Buffer)
-
-    Buffer = struct.pack('BBHHHBBHLL', IpVerLen, 0, IpTotal_len, 1, 0x40, 255,
-                         socket.IPPROTO_TCP, 0, IpSourceIP, IpDestIP)
-    IpChecksum = SetPacketCheckSum(Buffer)
-
-    tcpcsp = struct.pack('L', TcpChecksum)
-    ipcsp = struct.pack('L', IpChecksum)
-
-    return struct.pack('BBHHHBBHLLHHLLBBHHH', IpVerLen, 0, IpTotal_len, 1, 0x40, 255, socket.IPPROTO_TCP,
-                       (ord(ipcsp[1]) << 8) + ord(ipcsp[0]), IpSourceIP, IpDestIP,
-                       TcpSport, TcpDport, TcpSeq, 0, TcpLenres, 2, socket.htons(8192),
-                       (ord(tcpcsp[1]) << 8) + ord(tcpcsp[0]), 0)
+        ip_header = struct.pack('BBHHHBBH4s4s', ver_head_len, server, tot_len, id, flag_offset
+                                , ttl, protocol, check_sum, source_ip, dest_ip)
+        return ip_header
+    except Exception as e:
+        print('Error : {}'.format(e))
+        return None
 
 
-def SynSendMain(DestIP='127.0.0.1', DestPort=80):
-    return SendPacketData(SynSendInit(DestIP, DestPort), DestIP, DestPort)
+def GetTcpHeader(SourcePort, DestPort, CheckSum=0):
+    '''
+    获取TCP首部
+    :param SourcePort:源端口
+    :param DestPort:目标端口
+    :return:
+    '''
+    try:
+        # 16 源端口
+        source_port = socket.htons(SourcePort)
+        # 16 目的端口
+        dest_port = socket.htons(DestPort)
+        # 32 序号
+        seq = 0
+        # 32 确认号
+        ack_seq = 0
+        # 4 头部长度
+        head_len = 5
+        # 6 保留位
+        reserve = 0
+        # 6 TCP标志位
+        fin = 0
+        syn = 1
+        rst = 0
+        psh = 0
+        ack = 0
+        urg = 0
+        tcp_flag = fin + (syn << 1) + (rst << 2) + (psh << 3) + (ack << 4) + (urg << 5)
+        head_flag = (head_len << 12) + (reserve << 6) + tcp_flag
+
+        # 16 窗口大小
+        window = socket.htons(8192)
+        # 16 检验和
+        check_sum = CheckSum
+        # 16 紧急指针
+        urg_ptr = 0
+
+        tcp_header = struct.pack('HHIIHHHH', source_port, dest_port, seq, ack_seq, head_flag
+                                 , window, check_sum, urg_ptr)
+        return tcp_header
+
+    except Exception as e:
+        print('Error : {}'.format(e))
+        return None
+
+
+def GetCheckSum(Header=None):
+    '''
+    获取校验和
+    :param Header:
+    :return:
+    '''
+    if Header is None:
+        return None
+    if len(Header) % 2:
+        Header += b'0'
+    try:
+        all_sum = sum([(Header[n] << 8) + Header[n+1] for n in range(0, len(Header), 2)])
+        l_word = all_sum & 0x0000FFFF
+        h_word = all_sum >> 16
+        check_sum = l_word + h_word
+        check_sum2 = (~check_sum) & 0xFFFF
+        return check_sum2
+    except Exception as e:
+        print('Error : {}'.format(e))
+        return None
+
+
+def GetIPPacket(SourceIP, SourcePort, DestIP, DestPort):
+    '''
+    获取IP包
+    :param SourceIP:
+    :param SourcePort:
+    :param DestIP:
+    :param DestPort:
+    :return:
+    '''
+    # 构建TCP包
+    try:
+        tcp_packet = GetTcpHeader(SourcePort, DestPort)
+        tcp_checksum = GetCheckSum(tcp_packet)
+        if tcp_checksum is None:
+            return None
+        tcp_packet = GetTcpHeader(SourcePort, DestPort, tcp_checksum)
+        if tcp_checksum is None:
+            return None
+
+        # 构建IP包
+        ip_packet = GetIPHeader(SourceIP, DestIP)
+        ip_checksum = GetCheckSum(ip_packet)
+        if ip_checksum is None:
+            return None
+        ip_packet = GetIPHeader(SourceIP, DestIP, ip_checksum)
+        if ip_checksum is None:
+            return None
+
+        return ip_packet + tcp_packet
+    except Exception as e:
+        print('Error : {}'.format(e))
+        return None
+
+def IPUnpacket(Buffer):
+    ver_head_len, server, tot_len, id, flag_offset, ttl, protocol, check_sum, source_ip, \
+    dest_ip = struct.unpack('!BBHHHBBH4s4s', Buffer[:20])
+    head_len = ver_head_len & 0xF
+    s_ip = socket.inet_ntoa(source_ip)
+    d_ip = socket.inet_ntoa(dest_ip)
+    print('head_size:{}, all_size:{}, id:{}, ttl:{}, source_ip:{}, dest_ip:{}'.format(head_len, tot_len
+            ,id, ttl, s_ip, d_ip))
+    if tot_len < 40:
+        return None
+    source_port, dest_port, seq, ack_seq, head_flag, window, check_sum, urg_ptr = struct.unpack('!HHIIHHHH'
+                                                                                                ,Buffer[20:40])
+    head_len2 = head_flag >> 12
+    print('source_port:{}, dest_port:{}, seq:{}, ack_seq:{}, window:{}, head_len:{}'.format(source_port
+                                                                , dest_port, seq, ack_seq, window, head_len2))
+    if head_len == head_len2:
+        buff = Buffer[40:]
+        print('data:{}'.format(decodeall(buff)))
+
 
 if __name__ == '__main__':
-    SynSendMain()
+    sock = CreateSocket()
+    SourceIP = '192.168.100.69'
+    SourcePort = random.randint(1024, 65000)
+    DestIP = '192.168.100.71'
+    DestPort = 80
+    buff = GetIPPacket(SourceIP, SourcePort, DestIP, DestPort)
+    sock.sendto(buff, (DestIP, DestPort))
+    while True:
+        header, addr = sock.recvfrom(1024)
+        if header:
+            print('recv:{}'.format(addr))
+            IPUnpacket(header)
--- ScanPort.py
+++ ScanPort.py
@@ -16,7 +16,7 @@ def checksum(msg):
     s = 0
     # 每次取2个字节
     for i in range(0,len(msg),2):
-        w = (ord(msg[i]) << 8) + (ord(msg[i+1]))
+        w = (msg[i] << 8) + (msg[i+1])
         s = s+w
 
     s = (s>>16) + (s & 0xffff)
@@ -28,11 +28,11 @@ def CreateSocket(source_ip,dest_ip):
     try:
         s = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_TCP)
     except socket.error as msg:
-        print('Socket create error: ',str(msg[0]),'message: ',msg[1])
+        print('Socket create error: ',str(msg.errno),'message: ',msg.strerror)
         sys.exit()
 
     # 设置手工提供IP头部
-    s.setsockopt(socket.IPPROTO_TCP, socket.IP_HDRINCL, 1)
+    s.setsockopt(socket.IPPROTO_IP, socket.IP_HDRINCL, 1)
     return s
 
 # 创建IP头部
@@ -77,13 +77,13 @@ def create_tcp_syn_header(source_ip, dest_ip, dest_port):
     tcp_flags = fin + (syn<<1) + (rst<<2) + (psh<<3) + (ack<<4) + (urg<<5)
     tcp_header = pack('!HHLLBBHHH', source, dest_port, seq, ack_seq, offset_res, tcp_flags, window, check, urg_ptr)
     # 伪头部选项
-    source_address = socket.inet_aton( source_ip )
-    dest_address = socket.inet_aton( dest_ip )
+    source_address = socket.inet_aton(source_ip)
+    dest_address = socket.inet_aton(dest_ip)
     placeholder = 0
     protocol = socket.IPPROTO_TCP
     tcp_length = len(tcp_header)
-    psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length);
-    psh = psh + tcp_header;
+    psh = pack('!4s4sBBH', source_address, dest_address, placeholder, protocol, tcp_length)
+    psh = psh + tcp_header
     tcp_checksum = checksum(psh)
 
     # 重新打包TCP头部，并填充正确地校验和
@@ -94,15 +94,15 @@ def create_tcp_syn_header(source_ip, dest_ip, dest_port):
 def range_scan(source_ip, dest_ip, start_port, end_port) :
     syn_ack_received = []   # 开放端口存储列表
 
-    for j in range (start_port, end_port) :
+    for j in range(start_port, end_port):
         s = CreateSocket(source_ip, dest_ip)
         ip_header = CreateIpHeader(source_ip, dest_ip)
-        tcp_header = create_tcp_syn_header(source_ip, dest_ip,j)
+        tcp_header = create_tcp_syn_header(source_ip, dest_ip, j)
         packet = ip_header + tcp_header
 
-        s.sendto(packet, (dest_ip, 0))
+        s.sendto(packet, 0, (dest_ip, j))
 
-        data = s.recvfrom(1024) [0][0:]
+        data = s.recvfrom(1024)[0][0:]
 
         ip_header_len = (ord(data[0]) & 0x0f) * 4
         ip_header_ret = data[0: ip_header_len - 1]
@@ -114,22 +114,22 @@ def range_scan(source_ip, dest_ip, start_port, end_port) :
     return syn_ack_received
 
 if __name__ == '__main__':
-    pass
-    # open_port_list = []
-    # ipsource = '192.168.100.70'
-    # ipdest = '192.168.100.72'
-    # start = 100
-    # stop = 450
-    # step = int((stop - start) / 10)
-    # scan_ports = range(start, stop, step)
-    # if scan_ports[len(scan_ports) - 1] < stop:
-    #     scan_ports.append(stop)
-    # for i in range(len(scan_ports) - 1):
-    #     opl = range_scan(ipsource, ipdest, scan_ports[i], scan_ports[i + 1])
-    #     open_port_list.append(opl)
-    # for i in range(len(open_port_list)):
-    #     print('Process #: ', i, ' Open ports: ', open_port_list[i])
-    # print('A list of all open ports found: ')
-    # for i in range(len(open_port_list)):
-    #     for j in range(len(open_port_list[i])):
-    #         print(open_port_list[i][j], ', ')
+    # pass
+    open_port_list = []
+    ipsource = '192.168.100.70'
+    ipdest = '192.168.100.72'
+    start = 100
+    stop = 450
+    step = int((stop - start) / 10)
+    scan_ports = list(range(start, stop, step))
+    if scan_ports[len(scan_ports) - 1] < stop:
+        scan_ports.append(stop)
+    for i in range(len(scan_ports) - 1):
+        opl = range_scan(ipsource, ipdest, scan_ports[i], scan_ports[i + 1])
+        open_port_list.append(opl)
+    for i in range(len(open_port_list)):
+        print('Process #: ', i, ' Open ports: ', open_port_list[i])
+    print('A list of all open ports found: ')
+    for i in range(len(open_port_list)):
+        for j in range(len(open_port_list[i])):
+            print(open_port_list[i][j], ', ')
